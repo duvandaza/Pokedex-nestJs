a391868a73395f2fbd40f4a0e7190b6626c39ec4
Duvan Daza <duvandaxx@gmail.com>
2022-08-23 21:43:12 -0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,34 @@
+import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule } from '@nestjs/config';
+
+import { CommonModule } from './common/common.module';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { SeedModule } from './seed/seed.module';
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
+
+
+@Module({
+  imports: [
+
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
+    }),
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname,'..','public'),
+    }),
+    
+    MongooseModule.forRoot(process.env.MONGODB),
+    PokemonModule,
+    CommonModule,
+    SeedModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,14 @@
+import { IsInt, IsPositive, IsString, Min, MinLength } from "class-validator";
+
+export class CreatePokemonDto {
+
+    @Min(1)
+    @IsInt()
+    @IsPositive()
+    no: number;
+
+    @IsString()
+    @MinLength(1)
+    name: string;
+
+}
